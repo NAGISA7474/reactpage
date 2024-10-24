f8f0704b4fb26b330d9e37a654c13e4652d850ef
NAGISA7474 <220072rg@yse-c.net>
2024-10-24 12:48:30 +0900

    failed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,21 @@
 const Banner = () => {
-    return(
-      <div className="banner">
-        <div className="banner__contents">
-          <h1 className="banner__title">Ginny &amp; Georgia</h1>
-          <div className="banner__buttons">
-            <button className="banner__button">Play</button>
-            <button className="banner__button">My List</button>
-          </div>
-          <h1 className="banner__description">
-            Angsty and awkward fifteen year old Ginny Miller often feels more
-            mature than her thirty year old mother, the irresistible and dynamic
-            Georgia Miller...
-          </h1>
+  return(
+    <div className="banner">
+      <div className="banner__contents">
+        <h1 className="banner__title">Ginny &amp; Georgia</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
         </div>
-        <div className="banner--fadeBottom"></div>
+        <h1 className="banner__description">
+          Angsty and awkward fifteen year old Ginny Miller often feels more
+          mature than her thirty year old mother, the irresistible and dynamic
+          Georgia Miller...
+        </h1>
       </div>
-    )
-  }
-  
-  export default Banner;
+      <div className="banner--fadeBottom"></div>
+    </div>
+  )
+}
+
+export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,8 +19,8 @@ const Header = () => {
           <div className="navbar">
             <form className="d-flex" role="search">
               <select>
-                <option>日本語</option>
                 <option>English</option>
+                <option>Hindi</option>
               </select>
               <button className="btn btn-danger" onClick={clickHandler}>Signin</button>
             </form>
